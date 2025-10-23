707ace7b9c42338a5b45960d8ade4815a3d87664
tsao100 <tsao100@ms17.hinet.net>
2025-10-23 22:58:12 +0800

    Add wayland compatible.

--- src/main.cpp
+++ src/main.cpp
@@ -11,6 +11,17 @@
 #include "MainWindow.h"
 
 int main(int argc, char **argv) {
+#ifdef _WIN32
+    QApplication::setAttribute(Qt::AA_UseDesktopOpenGL);
+#else
+    // Force Qt to use XCB before QApplication is created
+    const char* session = std::getenv("XDG_SESSION_TYPE");
+
+    if (session && std::strcmp(session, "wayland") == 0) {
+        qDebug("Detected Wayland session → forcing xcb");
+        qputenv("QT_QPA_PLATFORM", QByteArray("xcb"));
+    }
+#endif
     QApplication app(argc, argv);
     MainWindow w;
 
